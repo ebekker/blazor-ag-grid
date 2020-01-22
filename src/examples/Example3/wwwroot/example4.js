55f93f5833a6f1bcc35587e39e1951426a5ba142
Eugene Bekker <ebekker@users.noreply.github.com>
2020-01-22 06:35:48 -0500

    adding Blazor server-side example

---
+++ src/examples/Example3/wwwroot/example4.js
@@ -0,0 +1,21 @@
+﻿// This file is to show how a library package may provide JavaScript interop features
+// wrapped in a .NET API
+
+window.example4_fetchData4_configure = function (gridOptions) {
+    gridOptions.getRowNodeId = function (node) {
+        var id = node.id;
+
+        return "ID#" + id;
+    }
+};
+
+window.example4_fetchData5_albums_configure = function (gridOptions) {
+    gridOptions.getRowNodeId = function (node) {
+        return "" + node.id;
+    }
+};
+window.example4_fetchData5_photos_configure = function (gridOptions) {
+    gridOptions.getRowNodeId = function (node) {
+        return "ID#" + node.id;
+    }
+};
